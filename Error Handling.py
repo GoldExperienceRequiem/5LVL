c4a0f6e84d6cd6884a268618002363776beb67bb
GoldExperienceRequiem <avrumel10@gmail.com>
2024-10-09 08:54:38 -0500

    Prep for IPE

--- Error Handling.py
+++
@@ -1,7 +0,0 @@
-#Demonstrate Error Handling
-
-try:
-    number = int(input("Enter an integer: "))
-    print(f"You entered: {number}")
-except ValueError:
-    print("Error: That's not a valid integer.")
